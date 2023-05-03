6ee48ba0de2e55758ce0090785ede3b961fd0877
SaMax408 <127476379+SaMax408@users.noreply.github.com>
2023-05-03 16:48:41 +0900

    modify: HomeJob.js 코드 재배치

--- question_maker/src/home/HomeJob.js
+++ question_maker/src/home/HomeJob.js
@@ -1,11 +1,11 @@
 function HomeJob() {
   return (
     <div className="job-container">    
-           <div className="job">
-              당신의 직무는? 
-           <div className="select-job">JOB</div>
-           </div>
-        </div>
+      <div className="job">
+        당신의 직무는? 
+        <div className="select-job">JOB</div>
+      </div>
+    </div>
   )
 }
 
