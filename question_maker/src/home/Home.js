4c41d630ab31e86ff01c4b1e82ce52d075662230
SaMax408 <127476379+SaMax408@users.noreply.github.com>
2023-05-03 18:33:23 +0900

    feat: mui container 적용

--- question_maker/src/home/Home.js
+++ question_maker/src/home/Home.js
@@ -5,14 +5,16 @@ import HomeQuestion from './HomeQuestion'
 
 import styles from './Home.css';
 
+import Container from '@mui/material/Container';
+
 function Home() {
   return (
-    <div>
+    <Container maxWidth="md">
       <HomeHeader></HomeHeader>
       <HomeJob></HomeJob>
       <HomeEssay></HomeEssay>
       <HomeQuestion></HomeQuestion>
-    </div>
+    </Container>
   )
 }
 
--- question_maker/src/home/HomeHeader.js
+++ question_maker/src/home/HomeHeader.js
@@ -1,9 +1,7 @@
 function HomeHeader() {
   return (
     <div>
-      <div className="main-screen">
-        <span className='title'>AI 면접 생성 질문기</span>
-      </div>
+      <div className='title'>AI 면접 생성 질문기</div>
     </div>
   )
 }
