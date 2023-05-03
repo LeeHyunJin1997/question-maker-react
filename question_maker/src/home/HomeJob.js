132e5af1f9550cc729d0b42886135966c6109c41
SaMax408 <127476379+SaMax408@users.noreply.github.com>
2023-05-03 19:51:15 +0900

    feat: Job selector 기능 구현, style 적용

--- question_maker/src/home/HomeJob.js
+++ question_maker/src/home/HomeJob.js
@@ -1,10 +1,32 @@
+import { useState } from 'react';
+import { Select, MenuItem, InputLabel, FormControl } from '@mui/material';
+
 function HomeJob() {
+  let [job, setJob] = useState(''); 
+
+  function handleChange(event) {
+    setJob(event.target.value);
+  }
+
   return (
-    <div className="job-container">    
+    <div>
       <div className="job">
-        당신의 직무는? 
-        <div className="select-job">JOB</div>
+        <div>직무를 선택해주세요</div>
+        <FormControl>
+          <InputLabel id="job-selector">직무</InputLabel>
+          <Select
+            labelId="job-selector"
+            label="직무"
+            value={job}
+            onChange={handleChange}
+          >
+            <MenuItem value={'기획'}>기획</MenuItem>
+            <MenuItem value={'마케팅'}>마케팅</MenuItem>
+            <MenuItem value={'회계'}>회계</MenuItem>
+          </Select>
+        </FormControl>
       </div>
+      <div>내 직무가 없다면?</div>
     </div>
   )
 }
