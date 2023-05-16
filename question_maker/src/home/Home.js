5d8a5a51dad3d6c440d021ff1f2927af565eb274
LeeHyunJin1997 <hjnee1102s@gmail.com>
2023-05-17 03:32:15 +0900

    feat: Add loading feature - 질문 생성 api 호출시 응답완료까지 로딩 화면 표출 - 응답 결과 개행 방식 변경, line-height 대신 div 간 margin 사용

--- question_maker/src/home/Home.js
+++ question_maker/src/home/Home.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 
+import Loading from './Loading';
 import HomeHeader from './HomeHeader';
 import HomeJob from './HomeJob';
 import HomeEssay from './HomeEssay';
@@ -13,6 +14,7 @@ import Container from '@mui/material/Container';
 const BASE_URL = process.env.REACT_APP_BASE_URL
 
 function Home() {
+  const [isLoading, setIsLoading] = useState(false)
   const [job, setJob] = useState("");
   const [essay, setEssay] = useState("");
   const [question, setQuestion] = useState("");
@@ -29,29 +31,35 @@ function Home() {
     setEssay(text)
   }
 
-  async function submitEssay() {
+  function submitEssay() {
+    setIsLoading(true);
+
     axios.post(BASE_URL + '/api/v1/question', {
       job: job,
       essay: essay,
     }).then((response) => {
       setQuestion(response.data)
+      setIsLoading(false)
     }).catch((error) => {
       console.log(error)
     })
   }
 
   return (
-    <Container maxWidth="md">
-      <HomeHeader></HomeHeader>
-      <HomeJob job={job} changeJob={changeJob}></HomeJob>
-      <HomeEssay
-        essay={essay}
-        resetEssay={resetEssay}
-        changeEssay={changeEssay}
-        submitEssay={submitEssay}
-      ></HomeEssay>
-      <HomeQuestion question={question}></HomeQuestion>
-    </Container>
+    <div>
+      {isLoading && <Loading></Loading>}
+      <Container maxWidth="md">
+        <HomeHeader></HomeHeader>
+        <HomeJob job={job} changeJob={changeJob}></HomeJob>
+        <HomeEssay
+          essay={essay}
+          resetEssay={resetEssay}
+          changeEssay={changeEssay}
+          submitEssay={submitEssay}
+        ></HomeEssay>
+        <HomeQuestion question={question}></HomeQuestion>
+      </Container>
+    </div>
   )
 }
 
--- question_maker/src/home/HomeQuestion.js
+++ question_maker/src/home/HomeQuestion.js
@@ -3,10 +3,22 @@ function HomeQuestion(props) {
     return text.substring(text.indexOf('1.'))
   }
 
+  function nToBr(text) {
+    return (
+      <div>
+        {text.split(/(?:\r\n|\r|\n)/g).map((line) => {
+          return (
+            <div className="each-question">{line}</div>
+          )
+        })}
+      </div>
+    )
+  }
+
   return (
     <div className="question">
       <div className="question-header">예상 질문</div>
-      <div className="question-body">{startFromQuestion1(props.question)}</div>
+      <div className="question-body">{nToBr(startFromQuestion1(props.question))}</div>
     </div>
   )
 }
